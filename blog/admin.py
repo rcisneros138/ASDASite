6b64371639856409b893b5d691b7440e7690ddd8
rcisneros138 <rcisneros138@gmail.com>
2016-11-04 19:10:23 -0500

    Revert "added first front end code"
    This reverts commit 2bad48398a2a037f0853d995f223b828d557165f.

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,9 @@ from blog.models import BlogPost
 
 admin.site.register(BlogPost)
 
-
 class BlogPostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if not change:
             obj.author = request.user
         obj.save()
+
--- blog/secrets.py
+++
@@ -1,1 +0,0 @@
-SecretKeys = {'ASDASecretKey' : 'wq7sjlslsjaoel&rn=4%k#$35@27+#$#r3-up5z2rb%1!w#+35', 'ASDABlogSecretKey' : 'ff3WHRtlqsvShNQ3Y54Robj5ONSyUXOvsRNm6CuefxpX3YaJuZ'}
