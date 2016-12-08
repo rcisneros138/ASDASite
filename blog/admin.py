411a364463555a5a9a8bd3fbc9e2beee93f04538
rcisneros138 <rcisneros138@gmail.com>
2016-12-07 19:05:17 -0600

    whole lot of stuff added to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from blog.models import BlogPost
 
-admin.site.register(BlogPost)
-
 
 class BlogPostAdmin(admin.ModelAdmin):
+    class Media:
+        js = (
+            '/media/tiny_mce/tinymce.min.js',
+            '/media/textArea.js',
+        )
+
+    def get_readonly_fields(self, request, obj=None):
+        if (obj):
+            if not request.user.is_superuser:
+
+                readonly_fields = self.model._meta.get_fields()
+                print(readonly_fields)
+        return self.readonly_fields
+
     def save_model(self, request, obj, form, change):
         if not change:
             obj.author = request.user
         obj.save()
+
+admin.site.register(BlogPost, BlogPostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=140)
-    body = models.TextField()
+    body = RichTextUploadingField()
     date = models.DateTimeField()
     author = models.ForeignKey(User, null=True, blank=True)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def approve(self):
+        self.approved = True
+        self.save()
